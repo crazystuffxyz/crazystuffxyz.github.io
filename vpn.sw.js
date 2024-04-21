a0b55419c9444da68ad6f8ce32aef079d90a86a9
Crazystuffxyz <125907466+crazystuffofficial@users.noreply.github.com>
2024-04-21 19:00:41 -0400

    Update vpn.sw.js

--- vpn.sw.js
+++ vpn.sw.js
@@ -20,14 +20,14 @@ function makeFile(name, contenttype, content){
     var response = await fetch(url);
     var thetext = await response.text();
     console.log(thetext);
-    thetext = await thetext.replace(/theserviceworkerscriptscope/g, self.registration.scope.slice(0, self.registration.scope.length - 1)).replace(/thebareservernodeurl/g, atob(self.registration.options.token));
+    thetext = await thetext.replace(/theserviceworkerscriptscope/g, self.localStorage.getItem("scopeforyouknow").slice(0, self.localStorage.getItem("scopeforyouknow").length - 1)).replace(/thebareservernodeurl/g, atob(self.localStorage.getItem("tokenforbare")));
     return thetext;
     } catch(e){
       return "";
     }
   }
-makeFile(self.registration.scope + "uv/uv.config.js", "application/javascript", getFromUrl(self.registration.scope + "vpn/uv.config.js"));
-makeFile(self.registration.scope + "uv/uv.bundle.js", "application/javascript", getFromUrl(self.registration.scope + "vpn/uv.bundle.js"));
-makeFile(self.registration.scope + "uv/uv.handler.js", "application/javascript", getFromUrl(self.registration.scope + "vpn/uv.config.js"));
-makeFile(self.registration.scope + "uv/uv.sw.js", "application/javascript", getFromUrl(self.registration.scope + "vpn/uv.sw.js"));
+makeFile(self.localStorage.getItem("scopeforyouknow") + "uv/uv.config.js", "application/javascript", getFromUrl(self.localStorage.getItem("scopeforyouknow") + "vpn/uv.config.js"));
+makeFile(self.localStorage.getItem("scopeforyouknow") + "uv/uv.bundle.js", "application/javascript", getFromUrl(self.localStorage.getItem("scopeforyouknow") + "vpn/uv.bundle.js"));
+makeFile(self.localStorage.getItem("scopeforyouknow") + "uv/uv.handler.js", "application/javascript", getFromUrl(self.localStorage.getItem("scopeforyouknow") + "vpn/uv.config.js"));
+makeFile(self.localStorage.getItem("scopeforyouknow") + "uv/uv.sw.js", "application/javascript", getFromUrl(self.localStorage.getItem("scopeforyouknow") + "vpn/uv.sw.js"));
 });
