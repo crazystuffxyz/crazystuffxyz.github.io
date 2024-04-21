5b4bb53b499acaeaa5021f5766021b6e634310bf
Crazystuffxyz <125907466+crazystuffofficial@users.noreply.github.com>
2024-04-21 18:36:03 -0400

    Update vpn.sw.js

--- vpn.sw.js
+++ vpn.sw.js
@@ -18,13 +18,16 @@ function makeFile(name, contenttype, content){
   async function getFromUrl(url){
     try{
     var response = await fetch(url);
-    return await response.text();
+    var thetext = await response.text();
+    console.log(thetext);
+    thetext = await thetext.replace(/theserviceworkerscriptscope/g, self.registration.scope.slice(0, self.registration.scope.length - 1)).replace(/thebareservernodeurl/g, atob(self.registration.options.token));
+    return thetext;
     } catch(e){
       return "";
     }
   }
-makeFile(self.registration.scope + "uv/uv.config.js", "application/javascript", getFromUrl(self.registration.scope + "vpn/uv.config.js").replace(/theserviceworkerscriptscope/g, self.registration.scope.slice(0, self.registration.scope.length - 1)).replace(/thebareservernodeurl/g, atob(self.registration.options.token)));
-makeFile(self.registration.scope + "uv/uv.bundle.js", "application/javascript", getFromUrl(self.registration.scope + "vpn/uv.bundle.js").replace(/theserviceworkerscriptscope/g, self.registration.scope.slice(0, self.registration.scope.length - 1)).replace(/thebareservernodeurl/g, atob(self.registration.options.token)));
-makeFile(self.registration.scope + "uv/uv.handler.js", "application/javascript", getFromUrl(self.registration.scope + "vpn/uv.config.js").replace(/theserviceworkerscriptscope/g, self.registration.scope.slice(0, self.registration.scope.length - 1)).replace(/thebareservernodeurl/g, atob(self.registration.options.token)));
-makeFile(self.registration.scope + "uv/uv.sw.js", "application/javascript", getFromUrl(self.registration.scope + "vpn/uv.sw.js").replace(/theserviceworkerscriptscope/g, self.registration.scope.slice(0, self.registration.scope.length - 1)).replace(/thebareservernodeurl/g, atob(self.registration.options.token)));
+makeFile(self.registration.scope + "uv/uv.config.js", "application/javascript", getFromUrl(self.registration.scope + "vpn/uv.config.js"));
+makeFile(self.registration.scope + "uv/uv.bundle.js", "application/javascript", getFromUrl(self.registration.scope + "vpn/uv.bundle.js"));
+makeFile(self.registration.scope + "uv/uv.handler.js", "application/javascript", getFromUrl(self.registration.scope + "vpn/uv.config.js"));
+makeFile(self.registration.scope + "uv/uv.sw.js", "application/javascript", getFromUrl(self.registration.scope + "vpn/uv.sw.js"));
 });
