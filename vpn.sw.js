b5c71e3700a0552181660440751eaf8382059967
Crazystuffxyz <125907466+crazystuffofficial@users.noreply.github.com>
2024-04-23 05:22:37 -0400

    Update vpn.sw.js

--- vpn.sw.js
+++ vpn.sw.js
@@ -8,13 +8,11 @@ self.addEventListener('fetch', function(event) {
     event.respondWith(
       fetch(scope + currenturl.replace(location.origin + scope, "").replace("uv/", "vpn/"))
         .then(function(response) {
-          console.log(scope + currenturl.replace(location.origin + scope, "").replace("uv/", "vpn/"));
           originalResponse = response;
           return response.text(); // return the promise here
         })
         .then(function(text) {
           var thetext = text;
-          console.log(thetext);
           thetext = thetext.replace(/theserviceworkerscriptscope/g, scope.slice(0, scope.length - 1)).replace(/thebareservernodeurl/g, token);
           // Create a new Response object with the fetched content
           return new Response(thetext, {
@@ -32,4 +30,29 @@ self.addEventListener('fetch', function(event) {
         })
     );
   }
+  
+  if (event.request.url.includes(scope + 'sw.js')) {
+    event.respondWith(
+      fetch(scope + 'dist.sw.js')
+        .then(function(response) {
+          originalResponse = response;
+          return response.text(); // return the promise here
+        })
+        .then(function(text) {
+          return new Response(text, {
+            headers: {
+              'Content-Type': originalResponse.headers.get('Content-Type')
+            }
+          });
+        })
+        .catch(function(error) {
+          // In case of an error, return a simple error response
+          return new Response('Error fetching content: ' + error, {
+            status: 500,
+            statusText: 'Internal Server Error'
+          });
+        })
+    );
+  }
+  
 });
