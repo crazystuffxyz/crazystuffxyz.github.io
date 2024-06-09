1f90ac43cc46b16fc17c5d81192afc0fd1e56b96
Crazystuffxyz <125907466+crazystuffofficial@users.noreply.github.com>
2024-06-09 08:36:16 -0400

    Update vpn.sw.js

--- vpn.sw.js
+++ vpn.sw.js
@@ -12,32 +12,31 @@ self.addEventListener('fetch', function(event) {
                     return cachedResponse;
                 }
 
-// Fetch the file from the network
-return fetch(scope + currenturl.replace(location.origin + scope, "").replace("uv/", "vpn/"))
-    .then(response => {
-        // Clone the response before reading its body
-        const responseToCache = response.clone();
+                // Fetch the file from the network
+                return fetch(scope + currenturl.replace(location.origin + scope, "").replace("uv/", "vpn/"))
+                    .then(response => {
+                        // Clone the response before reading its body
+                        const responseToCache = response.clone();
 
-        return response.text().then(text => {
-            var thetext = text;
-            thetext = thetext.replace(/theserviceworkerscriptscope/g, scope.slice(0, scope.length - 1))
-                             .replace(/thebareservernodeurl/g, token);
+                        return response.text().then(text => {
+                            var thetext = text;
+                            thetext = thetext.replace(new RegExp('theserviceworkerscriptscope', 'g'), scope.slice(0, scope.length - 1)).replace(new RegExp('thebareservernodeurl', 'g'), token);
+                            console.log(thetext);
+                            // Create a new Response object with the modified content
+                            const modifiedResponse = new Response(thetext, {
+                                headers: {
+                                    'Content-Type': response.headers.get('Content-Type')
+                                }
+                            });
 
-            // Create a new Response object with the modified content
-            const modifiedResponse = new Response(thetext, {
-                headers: {
-                    'Content-Type': response.headers.get('Content-Type')
-                }
-            });
+                            // Cache the modified response
+                            caches.open('dynamic-cache').then(cache => {
+                                cache.put(event.request, responseToCache);
+                            });
 
-            // Cache the modified response
-            caches.open('dynamic-cache').then(cache => {
-                cache.put(event.request, responseToCache);
-            });
-
-            return modifiedResponse;
-        });
-    })
+                            return modifiedResponse;
+                        });
+                    })
 
                     .catch(error => {
                         // In case of an error, return a simple error response
