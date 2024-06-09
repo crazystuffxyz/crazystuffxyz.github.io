fd6619b0c4fe78c52452eb33f25d68b123797059
Crazystuffxyz <125907466+crazystuffofficial@users.noreply.github.com>
2024-06-09 09:03:29 -0400

    Update vpn.sw.js

--- vpn.sw.js
+++ vpn.sw.js
@@ -20,25 +20,25 @@ self.addEventListener('fetch', function(event) {
         event.respondWith(
             caches.match(event.request).then(cachedResponse => {
                 if (cachedResponse) {
-                    // Return the cached response if available and exit
                     return cachedResponse;
                 }
 
-                // Fetch the file from the network
                 return fetch(scope + currenturl.replace(location.origin + scope, "").replace("uv/", "vpn/"))
                     .then(response => {
-                        return response.text().then(text => {
+                        // Clone the response before reading its body
+                        const responseToCache = response.clone();
+
+                        return responseToCache.text().then(text => {
                             var thetext = text;
                             thetext = thetext.replace(new RegExp('theserviceworkerscriptscope', 'g'), scope.slice(0, scope.length - 1)).replace(new RegExp('thebareservernodeurl', 'g'), token);
                             console.log(thetext);
-                            // Create a new Response object with the modified content
+
                             const modifiedResponse = new Response(thetext, {
                                 headers: {
                                     'Content-Type': response.headers.get('Content-Type')
                                 }
                             });
 
-                            // Cache the modified response
                             caches.open('dynamic-cache').then(cache => {
                                 cache.put(event.request, modifiedResponse.clone());
                             });
@@ -46,9 +46,7 @@ self.addEventListener('fetch', function(event) {
                             return modifiedResponse;
                         });
                     })
-
                     .catch(error => {
-                        // In case of an error, return a simple error response
                         console.error('Error fetching content:', error);
                         return new Response('Error fetching content: ' + error, {
                             status: 500,
