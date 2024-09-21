7ff54da0bb8aceb62a3f76fbf6e6f34bc0756770
Crazystuffxyz <125907466+crazystuffofficial@users.noreply.github.com>
2024-09-21 05:00:42 -0400

    Update dist.sw.js

--- dist.sw.js
+++ dist.sw.js
@@ -1,13 +1,13 @@
 try{
-function setUp(scope, bare){
-  importScripts(scope + "/uv/uv.bundle.js");
-  importScripts(scope + "./uv/uv.config.js");
-  importScripts(scope + "./uv/uv.sw.js");
-  __uv$config.prefix = __uv$config.prefix.replace("theserviceworkerscriptscope", scope);
-  __uv$config.config = __uv$config.config.replace("theserviceworkerscriptscope", scope);
-  __uv$config.bundle = __uv$config.bundle.replace("theserviceworkerscriptscope", scope);
-  __uv$config.handler = __uv$config.handler.replace("theserviceworkerscriptscope", scope);
-  __uv$config.sw = __uv$config.sw.replace("theserviceworkerscriptscope", scope);
+function setUp(thepath, bare){
+  importScripts(thepath + "/uv/uv.bundle.js");
+  importScripts(thepath + "./uv/uv.config.js");
+  importScripts(thepath + "./uv/uv.sw.js");
+  __uv$config.prefix = __uv$config.prefix.replace("theserviceworkerscriptscope", thepath);
+  __uv$config.config = __uv$config.config.replace("theserviceworkerscriptscope", thepath);
+  __uv$config.bundle = __uv$config.bundle.replace("theserviceworkerscriptscope", thepath);
+  __uv$config.handler = __uv$config.handler.replace("theserviceworkerscriptscope", thepath);
+  __uv$config.sw = __uv$config.sw.replace("theserviceworkerscriptscope", thepath);
   __uv$config.bare = bare;
   const sw = new UVServiceWorker(__uv$config);
   let userKey = new URL(location).searchParams.get('userkey');
@@ -27,10 +27,9 @@ function setUp(scope, bare){
     }
   });
 }
-var url = new URLSearchParams(self.location.search);
-var scope = atob(url.get("scope"));
+var url = new URLSearchParams(self.location.search);=
 var bare = atob(url.get("bare"));
-setUp(scope, bare);
+setUp(atob(url.get("scope")), bare);
 } catch(e){
   console.log(e.message);
 }
