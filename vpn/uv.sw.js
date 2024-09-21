5320a1e9313e7e6f6833630160560303c214b3c6
Crazystuffxyz <125907466+crazystuffofficial@users.noreply.github.com>
2024-09-21 03:44:44 -0400

    Update uv.sw.js

--- vpn/uv.sw.js
+++ vpn/uv.sw.js
@@ -2,7 +2,8 @@ importScripts('uv/uv.bundle.js');
 importScripts('uv/uv.config.js');
 
 class UVServiceWorker extends EventEmitter {   
-    constructor(config) {
+    constructor(config) {]
+        if (!config) config = __uv$config;
         super();
         if (!config.bare) config.bare = '/bare/';
         this.addresses = typeof config.bare === 'string' ? [ new URL(config.bare, location) ] : config.bare.map(str => new URL(str, location));
